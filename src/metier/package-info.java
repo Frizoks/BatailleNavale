aeb41adf303756a65c9f1312435980004327adac
Frizoks <130101313+Frizoks@users.noreply.github.com>
2023-11-10 17:52:32 +0100

    Delete src directory

--- src/metier/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Ce package contient toutes les classes permettant de stocker les données, vérifier les placements et les tirs, et enfin de jouer à notre Bataille Navale.
- * 
- * @author Luc LECARPENTIER & Mathys PORET - BUT INFO - 2023
- */
-package metier;
--- src/serveur/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Ce package contient toutes les classes permettant la connexion et l'envoie des données entre les deux joueurs.
- * 
- * @author Luc LECARPENTIER & Mathys PORET - BUT INFO - 2023
- */
-package serveur;
