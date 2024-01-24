c6ee6cd19fb4b435b757b04e177e6319633755cd
Luc Lecarpentier <130101313+Frizoks@users.noreply.github.com>
2024-01-24 12:03:47 +0100

    Delete src directory

--- src/ihm/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Ce package contient toutes les classes permettant de construire les frames et de les rendres fonctionnelles.
- * 
- * @author Luc LECARPENTIER & Mathys PORET - BUT INFO - 2023
- */
-package ihm;
